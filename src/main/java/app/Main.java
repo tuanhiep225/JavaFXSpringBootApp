dcc58eee9a50f53d7adec09aa24e5eea5c3b1214
jhon <jhon@192.168.0.120>
2019-03-25 17:08:09 +0700

    bo file lib jar

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,20 +3,28 @@ package app;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ResourceBundle;
 import java.util.concurrent.Executor;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import app.config.CustomAsyncExceptionHandler;
+import app.config.SpringFXMLLoader;
 import app.config.StageManager;
+import app.logging.ExceptionWriter;
 import app.view.FxmlView;
 
 @SpringBootApplication
@@ -61,5 +69,30 @@ public class Main extends Application{
         String[] args = getParameters().getRaw().stream().toArray(String[]::new);
         return builder.run(args);
     }
+    
+    @Autowired 
+    SpringFXMLLoader springFXMLLoader;
+
+    /**
+     * Useful when dumping stack trace to a string for logging.
+     * @return ExceptionWriter contains logging utility methods
+     */
+    @Bean
+    @Scope("prototype")
+    public ExceptionWriter exceptionWriter() {
+        return new ExceptionWriter(new StringWriter());
+    }
+
+    @Bean
+    public ResourceBundle resourceBundle() {
+        return ResourceBundle.getBundle("Bundle");
+    }
+    
+    @Bean
+    @Lazy(value = true) //Stage only created after Spring context bootstap
+    public StageManager stageManager(Stage stage) throws IOException {
+        return new StageManager(springFXMLLoader, stage);
+    }
+
 
 }
